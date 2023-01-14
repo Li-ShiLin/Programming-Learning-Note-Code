c346c4f729e6e774d1192c76eb7af70d09bbf780
lsl <2241876453@qq.com>
2023-01-14 15:06:08 +0800

    优惠券秒杀  一人一单

--- 003_Redis/03_hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ 003_Redis/03_hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -30,6 +30,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
+
+    // 加锁解决并发问题
     @Override
     public Result seckillVoucher(Long voucherId) {
         // 1.查询优惠券
@@ -124,5 +126,85 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         return Result.ok(orderId);
 
     }
+
+
+/*
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1.查询优惠券
+        SeckillVoucher voucher = iSeckillVoucherService.getById(voucherId);
+        // 2.判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 尚未开始
+            return Result.fail("秒杀尚未开始！");
+        }
+        // 3.判断秒杀是否已经结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // 尚未开始
+            return Result.fail("秒杀已经结束！");
+        }
+
+        // 4.判断库存是否充足
+        if (voucher.getStock() < 1) {
+            //库存不足
+            return Result.fail("库存不足");
+        }
+
+        // 5.一人一单
+        Long userId = UserHolder.getUser().getId();
+        // 5.1 查询订单
+        Integer count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if(count > 0){
+            // 用户已经购买过了
+            return Result.fail("已经抢购成功，不可重复抢购！");
+        }
+
+*//*
+        // 6.扣减库存
+        boolean isSuccess = iSeckillVoucherService.update()
+                .setSql("stock = stock -1")
+                .eq("voucher_id", voucherId).update();*//*
+
+     *//*        // 6.扣减库存(解决超卖问题)
+        boolean isSuccess = iSeckillVoucherService.update()
+                .setSql("stock = stock -1")  // set stock = stock -1
+                .eq("voucher_id", voucherId)
+                .eq("stock",voucher.getStock())  // where id = ? and stock = ?
+                .update();*//*
+
+        // 6.扣减库存(解决超卖问题) 进一步优化——>提高抢购成功率
+        boolean isSuccess = iSeckillVoucherService.update()
+                .setSql("stock = stock -1")  // set stock = stock -1
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)  // where id = ? and stock > 0
+                .update();
+
+        if (!isSuccess) {
+            // 扣减失败
+            return Result.fail("库存扣减失败！");
+        }
+
+        // 7.创建订单(订单信息：订单id、用户id、代金券id)
+        VoucherOrder voucherOrder = new VoucherOrder();
+
+        // 订单id
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+
+        // 用户id
+        voucherOrder.setUserId(userId);
+
+        // 代金券id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+
+        // 8.返回订单id
+        return Result.ok(orderId);
+    }
+
+
+   */
 
 }
